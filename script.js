beb16fa58463a0e66eee2e2b62f009527236d7e6
Ravy Thach <rthach17@gmail.com>
2022-12-12 16:43:49 -0800

    Create 16 columns of 16 square divs

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let container = document.querySelector('div');
 
-for (let index = 0; index < 16; index++) {
-  const div = document.createElement('div');
-  div.classList.add('square');
-  container.appendChild(div);
+for (let i = 0; i < 16; i++) {
+  const columnDiv = document.createElement('div');
+  columnDiv.classList.add('column');
+  for (let j = 0; j < 16; j++) {
+    const squareDiv = document.createElement('div');
+    squareDiv.classList.add('square');
+    columnDiv.appendChild(squareDiv);
+  }
+  
+  container.appendChild(columnDiv);
 }
