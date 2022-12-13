ab1cd76c26bb17812b838a3fbfa46254e16631d0
Ravy Thach <rthach17@gmail.com>
2022-12-12 16:52:28 -0800

    Add functionality to change color when mouse hovers over grid

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
+    squareDiv.addEventListener('mouseover', function() {
+      this.classList.add('hover');
+    })
     columnDiv.appendChild(squareDiv);
   }
   container.appendChild(columnDiv);
