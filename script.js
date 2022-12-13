f1e933c33d84242dd754764833ab13ddb9389bce
Ravy Thach <rthach17@gmail.com>
2022-12-12 22:05:25 -0800

    Add createGrid() function

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 // Create 16x16 square grid
 let container = document.querySelector('div');
-for (let i = 0; i < 16; i++) {
-  const columnDiv = document.createElement('div');
-  columnDiv.classList.add('column');
-  for (let j = 0; j < 16; j++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-    squareDiv.addEventListener('mouseover', function() {
-      this.classList.add('hover');
-    })
-    columnDiv.appendChild(squareDiv);
+
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
+    const columnDiv = document.createElement('div');
+    columnDiv.classList.add('column');
+    for (let j = 0; j < num; j++) {
+      const squareDiv = document.createElement('div');
+      squareDiv.classList.add('square');
+      squareDiv.addEventListener('mouseover', function() {
+        this.classList.add('hover');
+      })
+      columnDiv.appendChild(squareDiv);
+    }
+    container.appendChild(columnDiv);
   }
-  container.appendChild(columnDiv);
 }
 
-// Reset square colors
+// Reset button to reset square colors
 let reset = document.getElementById('reset');
 reset.addEventListener('click', () => {
   let squares = document.querySelectorAll('.square');
   squares.forEach(square => square.classList.remove('hover'));
 });
+
+createGrid(8);
