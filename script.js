4fbc4f9bd09ad0377328e1bdc805bf39d07fd9e9
Ravy Thach <rthach17@gmail.com>
2022-12-12 22:23:24 -0800

    Add removeGrid() function and functionality to 'Change Grid Size' button

--- script.js
+++ script.js
@@ -17,11 +17,32 @@ function createGrid(num) {
   }
 }
 
-// Reset button to reset square colors
+function removeGrid() {
+  while (container.firstChild) {
+    let square = container.firstChild;
+    container.removeChild(square);
+  }
+}
+
+function promptGridChange() {
+  let num = prompt("Number of squares per side of grid", 16);
+  if (num > 100 || num < 1) {
+    promptGridChange();
+  } else {
+    removeGrid();
+    createGrid(num);
+  }
+}
+
+// 'Reset' button to reset square colors
 let reset = document.getElementById('reset');
 reset.addEventListener('click', () => {
   let squares = document.querySelectorAll('.square');
   squares.forEach(square => square.classList.remove('hover'));
 });
 
-createGrid(8);
+// 'Change Grid Size' button
+let change = document.querySelector('button');
+change.addEventListener('click', () => promptGridChange());
+
+createGrid(16);
